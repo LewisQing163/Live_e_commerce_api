c68a9515cbb826df9f340e431996f536c1689c24
Lewis.Qing <lq17520369303>
2021-12-24 22:19:54 +0800

    商品条件查询及分页

--- src/Live_e_commerce.Application.Contracts/Iservice/ICommodityService.cs
+++ src/Live_e_commerce.Application.Contracts/Iservice/ICommodityService.cs
@@ -3,16 +3,23 @@ using Live_e_commerce.Dto;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using Volo.Abp.Application.Dtos;
 using Volo.Abp.Application.Services;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Live_e_commerce.Iservice
 {
     /// <summary>
     /// 商品接口
     /// </summary>
-    public interface ICommodityService:ICrudAppService<CommodityDto,Guid,PagedAndSortedResultRequestDto, CreateUpdateCommodityDto>
+    public interface ICommodityService:ICrudAppService<CommodityDto,Guid,PagedAndSortedResultRequestDto, CreateUpdateCommodityDto>,IApplicationService
     {
+        /// <summary>
+        /// 自定义列表接口,带分页
+        /// </summary>
+        /// <returns></returns>
+        Task<IEnumerable<CommodityDto>> GetCommodity(string key, int pagesize=6, int pageindex=1);
 
     }
 }
--- src/Live_e_commerce.Application/Service/CommodityService.cs
+++ src/Live_e_commerce.Application/Service/CommodityService.cs
@@ -1,6 +1,8 @@
-﻿using Live_e_commerce.CreateUpdateDto;
+﻿using AutoMapper.Configuration;
+using Live_e_commerce.CreateUpdateDto;
 using Live_e_commerce.Dto;
 using Live_e_commerce.Entities;
+using Live_e_commerce.Iservice;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,8 +17,31 @@ namespace Live_e_commerce.Service
     /// <summary>
     /// 商品实现
     /// </summary>
-    public class CommodityService: CrudAppService<Commodity, CommodityDto,Guid,PagedAndSortedResultRequestDto,CreateUpdateCommodityDto>
+    public class CommodityService: CrudAppService<Commodity, CommodityDto,Guid,PagedAndSortedResultRequestDto,CreateUpdateCommodityDto>, ICommodityService
     {
-        public CommodityService(IRepository<Commodity,Guid> repository) : base(repository) { }
+        private IRepository<Commodity, Guid> _repository;
+        public CommodityService(IRepository<Commodity,Guid> repository) : base(repository) 
+        {
+            _repository = repository;
+        }
+        /// <summary>
+        /// 列表实现,分页
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="pagesize"></param>
+        /// <param name="pageindex"></param>
+        /// <returns></returns>
+        public async Task<IEnumerable<CommodityDto>> GetCommodity(string key, int pagesize=6, int pageindex=1)
+        {
+            var data = await _repository.GetListAsync();
+            if (!string.IsNullOrEmpty(key))
+            {
+                data= data.Where(p => p.Name.Contains(key)).ToList();
+            }
+            //var count = data.Count();
+            var a= ObjectMapper.Map<List<Commodity>, List<CommodityDto>>(data);
+            var list = a.Skip((pageindex - 1) * pagesize).Take(pagesize).ToList();
+            return list;
+        }
     }
 }
